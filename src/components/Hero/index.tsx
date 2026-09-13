926d2572d9b60ef97f04761f44bdd2338621b264
Henrique dos Santos <henrydossantos1560@gmail.com>
2026-09-13 11:05:25 -0300

    feat; adiciona componente de botões

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,4 +1,5 @@
 import Banner from "@/assets/images/banner.jpg"
+import { Button } from "../Button"
 
 export const Hero = () => {
     return (
@@ -15,8 +16,14 @@ export const Hero = () => {
                         <h1 className="text-2xl leading-9 tracking-widest mb-10">Transforme qualquer passo em presença</h1>
 
                         <div className="flex gap-3.5">
-                            <button>Ver modelos</button>
-                            <button>Comprar</button>
+
+                            <Button variant="secundary" size="sm">
+                                Ver modelos
+                            </Button>
+
+                            <Button variant="primary" size="md">
+                                Comprar
+                            </Button>
                         </div>
                     </div>
                 </div>
